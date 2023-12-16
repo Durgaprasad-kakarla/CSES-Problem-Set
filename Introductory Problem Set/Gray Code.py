6b1580b51c76fdbf8b58a2173736d33166b678fa
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2023-12-16 18:54:57 +0530

    Add files via upload
    Time Complexity--O(2^n)
    Space Complexity--O(bin(n))

---
+++ Introductory Problem Set/Gray Code.py
@@ -0,0 +1,25 @@
+
+def int_to_binary(n,x):
+    s=''
+    if n==0:
+        return '0'*x
+    while n>1:
+        rem=n%2
+        s=str(rem)+s
+        n=n//2
+    if len(s)<x:
+        s='0'*(x-len(s)-1)+'1'+s
+    return s
+def binary_to_gray(s):
+    st=s[0]
+    n=len(s)
+    for i in range(1,n):
+        st+=str(int(s[i-1])^int(s[i]))
+    return st
+n=int(input())
+for i in range(2**n):
+    s=int_to_binary(i,n)
+    s=binary_to_gray(s)
+    print(s)
+
+
