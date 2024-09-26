13a41d4f7b21554d7be6ebe96ad5d6e122e0cba6
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-09-26 18:20:53 +0530

    Accepted
    Time Complexity--O(nlogn)
    Space Complexity--O(1)

--- Sorting and Searching/Array Division.py
+++ Sorting and Searching/Array Division.py
@@ -1,20 +1,27 @@
-n,k=map(int,input().split())
-nums=list(map(int,input().split()))
-l,r = max(nums), sum(nums)
-while l < r:
-    mid = (l+r)//2
-    tot, cnt = 0, 1
-    for num in nums:
-        if tot+num<=mid:
-            tot += num
+n, k = map(int, input().split())
+nums = list(map(int, input().split()))
+ 
+def func(nums,ele):
+    sm,n,cnt=0,len(nums),0
+    for i in range(n):
+        if sm+nums[i]<=ele:
+            sm+=nums[i]
         else:
-            tot = num
-            cnt += 1
-    if cnt>k:
-         l= mid+1
-    else:
-        r= mid
-print(r)
-'''Time Complexity--o(nlogn)
-   Space Complexity--O(1)'''
-'''Time Limit Exceeded in CSES but worked in Codeforces'''
+            cnt+=1
+            sm=nums[i]
+    if sm>0:
+        cnt+=1
+    return cnt
+ 
+def array_division(nums,k):
+    n=len(nums)
+    l,r=max(nums),sum(nums)
+    while l<=r:
+        mid=(l+r)//2
+        if func(nums,mid)<=k:
+            r=mid-1
+        else:
+            l=mid+1
+    return l
+ 
+print(array_division(nums,k))
